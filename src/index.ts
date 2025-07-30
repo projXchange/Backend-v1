664040a517e7367c16af50d1c6e9700805372d7a
Nishant Patil <nishant@nxtjob.ai>
2025-07-31 00:41:52 +0530

    TASK: vercel fix- dev mode added

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import { Hono } from "hono";
-import { serve } from "@hono/node-server";
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello API!"));
+app.get('/', (c) => c.text('Hello API!'));
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-});
-console.log("Server is running on port 3000, can visit http://localhost:3000");
+// This part is for local development. It will not run on Vercel.
+// Vercel sets NODE_ENV to 'production' by default.
+if (process.env.NODE_ENV === 'development') {
+  serve({
+    fetch: app.fetch,
+    port: 3000,
+  });
+  console.log('Server is running on port 3000, can visit http://localhost:3000');
+}
+
 export default app;
-
